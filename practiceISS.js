34f2bc6e6c591ca1d68622a7733b12ce47fb0ea1
fabulousSage <fabuloussage@yahoo.com>
2021-01-25 06:59:38 +0100

    dummy API commit

---
+++ practiceISS.js
@@ -0,0 +1,13 @@
+
+
+async function getISS() {
+    const iss_url = 'https://api.wheretheiss.at/v1/satellites/25544'
+    const response = await fetch(iss_url);
+    const data = await response.json();
+    const { latitude, longitude } = data;
+
+    document.getElementById('lat').textContent = latitude
+    document.getElementById('long').textContent = longitude
+}
+
+(async () => { await getISS() })()
